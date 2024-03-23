b293cbaaee5161643a056b1874f52aa4f9bc9931
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-22 23:57:03 -0500

    Finish Project

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,61 @@
+class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kittens }
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "Kitten successfully updated"
+    else
+      flash.now[:error] = "Wow, there's errors. That's bad"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to kitten_path(@kitten), notice: 'Kitten Successfully Created'
+    else
+      flash.now[:error] = "Wow, there's errors. That's bad"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to kittens_path, notice: "Kitten was succesfully deleted"
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,5 @@
 class Kitten < ApplicationRecord
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 25, message: "must be between 0 and 25" }
+  validates :cuteness, :softness, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10, message: "must be between 0 and 10" }
+
 end
